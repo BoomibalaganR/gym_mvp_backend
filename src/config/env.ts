445b35f48f6d852f7c07c6346b3d7344bbfdd499
Boomibalagan-R <boomibalagan.r@digicoffer.com>
2025-11-14 16:26:47 +0530

    fix .env file

--- src/config/env.ts
+++ src/config/env.ts
@@ -2,9 +2,8 @@ import dotenv from 'dotenv';
 import path from 'path';
 
 dotenv.config();
-
 export const config = {
-  port: Number(process.env.PORT || 5000),
+  port: Number(process.env.PORT),
   mongoUri: process.env.MONGO_URI || 'mongodb://localhost:27017/gym_mvp',
   jwtSecret: process.env.JWT_SECRET || 'change_this_secure_value',
   jwtExpiresIn: process.env.JWT_EXPIRES_IN || '7d',
@@ -14,7 +13,7 @@ export const config = {
   smtpPass: process.env.SMTP_PASS || '',
   appEmail: process.env.APP_EMAIL || 'no-reply@example.com',
 
-   project_id: 'vitagist-consumer-docu',//process.env.FIREBASE_PROJECT_ID,
-    bucket_name: 'vitagist-consumer-docu.appspot.com',//process.env.FIREBASE_BUCKET_NAME,
+   project_id: process.env.FIREBASE_PROJECT_ID,
+    bucket_name:process.env.FIREBASE_BUCKET_NAME,
     key_file: path.resolve(process.cwd(), 'serviceAccountKey.json'),
 };
