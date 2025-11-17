6cb3163aee69b742b0d452d4f59ae7d0408f07eb
Boomibalagan-R <boomibalagan.r@digicoffer.com>
2025-11-17 17:13:39 +0530

    add twillo credential

--- src/config/env.ts
+++ src/config/env.ts
@@ -4,16 +4,23 @@ import path from 'path';
 dotenv.config();
 export const config = {
   port: Number(process.env.PORT),
+  node_env: process.env.NODE_ENV, 
+  
   mongoUri: process.env.MONGO_URI || 'mongodb://localhost:27017/gym_mvp',
   jwtSecret: process.env.JWT_SECRET || 'change_this_secure_value',
   jwtExpiresIn: process.env.JWT_EXPIRES_IN,
+  
   smtpHost: process.env.SMTP_HOST || '',
   smtpPort: Number(process.env.SMTP_PORT || 587),
   smtpUser: process.env.SMTP_USER || '',
   smtpPass: process.env.SMTP_PASS || '',
-  appEmail: process.env.APP_EMAIL || 'no-reply@example.com',
-    node_env: process.env.NODE_ENV,
-   project_id: process.env.FIREBASE_PROJECT_ID,
-    bucket_name:process.env.FIREBASE_BUCKET_NAME,
-    key_file: path.resolve(process.cwd(), 'serviceAccountKey.json'),
+  appEmail: process.env.APP_EMAIL || 'no-reply@example.com',  
+  
+  twilioAccountSid: process.env.TWILIO_ACCOUNT_SID || '',
+  twilioAuthToken: process.env.TWILIO_AUTH_TOKEN || '',
+  twilioPhoneNumber: process.env.TWILIO_PHONE_NUMBER || '',
+ 
+  project_id: process.env.FIREBASE_PROJECT_ID,
+  bucket_name:process.env.FIREBASE_BUCKET_NAME,
+  key_file: path.resolve(process.cwd(), 'serviceAccountKey.json'),
 };
